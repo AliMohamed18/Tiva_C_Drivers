866b10800d0d2706eb549615d9182f1d729dc922
Ali <alimoh18499@gmail.com>
2020-09-10 00:16:23 +0200

    Update Port_Types.h

--- Src/Mcal/Includes/Port_Types.h
+++ Src/Mcal/Includes/Port_Types.h
@@ -86,210 +86,188 @@ typedef enum
 	OPENDRAIN
 }Port_PinInternalAttachType;
 
-typedef enum
-{
-/********************************************************	PORTA	********************************************************/	
-DIO,	
-//A0
-U0Rx,    
-CAN1Rx,
-//A1
-U0Tx,
-CAN1Tx,
-//A2	
-A2_SSI0Clk,
-	
-A3_SSI0Fss,	
-	
-A4_SSI0Rx,
+typedef uint8 Port_PinModeType;
+#define DIO							 0
 
-A5_SSI0Tx,
-	
-A6_I2C1SCL,
-A6_M1PWM2,	
-	
-A7_I2C1SDA,
-A7_M1PWM3,	
-/********************************************************	PORTB	********************************************************/	
-B0_USB0ID,
-B0_U1Rx,
-B0_T2CCP0,
+#define A0_U0RX				 	 1
+#define A1_U0TX				 	 1
 
-B1_USB0VBUS,
-B1_U1Tx,
-B1_T2CCP1,
+#define A2_SSI0Clk			 2
+#define A3_SSI0Fss			 2
+#define A4_SSI0Rx			   2
+#define A5_SSI0Tx			   2
+ 
+#define A6_I2C1SCL			 3
+#define A7_I2C1SDA			 3
+ 
+#define A6_M1PWM2			 	 5
+#define A7_M1PWM3				 5
 
-B2_I2C0SCL,
-B2_T3CCP0,
+#define A0_CAN1Rx			 	 8
+#define A1_CAN1Tx			   8
 
-B3_I2C0SDA,
-B3_T3CCP1,
 
-B4_AIN10,
-B4_SSI2Clk,
-B4_M0PWM2,
-B4_T1CCP0,
-B4_CAN0Rx,
+#define B0_U1Rx				 	 1
+#define B1_U1Tx				 	 1
 
-B5_AIN11,
-B5_SSI2Fss,
-B5_M0PWM3,
-B5_T1CCP1,
-B5_CAN0Tx,
+#define B4_SSI2Clk			 2
+#define B5_SSI2Fss			 2
+#define B6_SSI2Rx			   2
+#define B7_SSI2Tx			   2
 
-B6_SSI2Rx,
-B6_M0PWM0,
-B6_T0CCP0,
-	
-B7_SSI2Tx,
-B7_M0PWM1,
-B7_T0CCP1,
-/********************************************************	PORTC	********************************************************/	
-C0_TCK_SWCLK,
-C0_T4CCP0,
+#define B2_I2C0SCL			 3
+#define B3_I2C0SDA			 3
 
-C1_TMS_SWDIO,
-C1_T4CCP1,
+#define B4_M0PWM2		 	   4
+#define B5_M0PWM3			   4 
+#define B6_M0PWM0			   4
+#define B7_M0PWM1			   4
 
-C2_TDI,
-C2_T5CCP0,
+#define B0_T2CCP0			   7
+#define B1_T2CCP1			   7
+#define B2_T3CCP0			   7
+#define B3_T3CCP1			   7
+#define B4_T1CCP0			   7
+#define B5_T1CCP1			   7
+#define B6_T0CCP0			   7
+#define B7_T0CCP1			 	 7
 
-C3_TDO_SWO,
-C3_T5CCP1,
+#define B4_CAN0Rx			   8
+#define B5_CAN0Tx			 	 8
 
-C4_C1Neg,
-C4_U4Rx,
-C4_U1Rx,
-C4_M0PWM6,
-C4_IDX1,
-C4_WT0CCP0,
-C4_U1RTS,
+ 
+#define C0_TCK_SWCLK		 1
+#define C1_TMS_SWDIO		 1
+#define C2_TDI					 1
+#define C3_TDO_SWO			 1	
+#define C4_U4Rx				   1
+#define C5_U4Tx				   1
+#define C6_U3Rx				 	 1
+#define C7_U3Tx				 	 1
 
-C5_C1Pos,
-C5_U4Tx,
-C5_U1Tx,
-C5_M0PWM7,
-C5_PhA1,
-C5_WT0CCP1,
-C5_U1CTS,
+#define C4_M0PWM2			 	 4
+#define C5_M0PWM3			 	 4
+#define C6_M0PWM0		 	   4
+#define C7_M0PWM1			 	 4
 
-C6_C0Pos,
-C6_U3Rx,
-C6_PhB1,
-C6_WT1CCP0,
-C6_USB0EPEN,
+#define C4_IDX1				 	 6
+#define C5_PhA1				 	 6
+#define C6_PhB1					 6
 
-C7_C0Neg,
-C7_U3Tx,
-C7_WT1CCP1,
-C7_USB0PFLT,
-/********************************************************	PORTD	********************************************************/	
-D0_AIN7,
-D0_SSI3Clk,
-D0_SSI1Clk,
-D0_I2C3SCL,
-D0_M0PWM6,
-D0_M1PWM0,
-D0_WT2CCP0,
+#define C0_T4CCP0			 	 7
+#define C1_T4CCP1			   7
+#define C2_T5CCP0			   7 
+#define C3_T5CCP1			   7  	
+#define C4_WT0CCP0			 7
+#define C5_WT0CCP1			 7
+#define C6_WT1CCP0		   7
+#define C7_WT1CCP1			 7
 
-D1_AIN6,
-D1_SSI3Fss,
-D1_SSI1Fss,
-D1_I2C3SDA,
-D1_M0PWM7,
-D1_M1PWM1,
-D1_WT2CCP1,
+#define C4_U1RTS				 8
+#define C5_U1CTS				 8
+#define C6_USB0EPEN		 	 8
+#define C7_USB0PFLT		 	 8
 
-D2_AIN5,
-D2_SSI3Rx,
-D2_SSI1Rx,
-D2_M0FAULT0,
-D2_WT3CCP0,
-D2_USB0EPEN,
 
-D3_AIN4,
-D3_SSI3Tx,
-D3_SSI1Tx,
-D3_IDX0,
-D3_WT3CCP1,
-D3_USB0PFLT,
+#define D0_SSI3Clk			 1
+#define D1_SSI3Fss			 1
+#define D2_SSI3Rx			   1
+#define D3_SSI3Tx			   1
+#define D4_U6Rx				   1
+#define D5_U6Tx				   1 	
+#define D6_U2Rx				 	 1
+#define D7_U2Tx					 1
 
-D4_USB0DM,
-D4_U6Rx,
-D4_WT4CCP0,
+#define D0_SSI1Clk			 2
+#define D1_SSI1Fss			 2
+#define D2_SSI1Rx				 2
+#define D3_SSI1Tx				 2
 
-D5_USB0DP,
-D5_U6Tx,
-D5_WT4CCP1,
+#define D0_I2C3SCL			 3
+#define D1_I2C3SDA			 3
 
-D6_U2Rx,
-D6_M0FAULT0,
-D6_PhA0,
-D6_WT5CCP0,
+#define D0_M0PWM6			 	 4
+#define D1_M0PWM7				 4
+#define D2_M0FAULT0		 	 4
 
-D7_U2Tx,
-D7_PhB0,
-D7_WT5CCP1,
-D7_NMI,
-/********************************************************	PORTE	********************************************************/	
-E0_AIN3,
-E0_U7Rx,
+#define D0_M1PWM0				 5
+#define D1_M1PWM1			   5
 
-E1_AIN2,
-E1_U7Tx,
+#define D3_IDX0				 	 6
+#define D6_PhA0 				 6
+#define D7_PhB0				   6
 
-E2_AIN1,
+#define D0_WT2CCP0			 7
+#define D1_WT2CCP1			 7
+#define D2_WT3CCP0			 7
+#define D3_WT3CCP1			 7
+#define D4_WT4CCP0			 7
+#define D5_WT4CCP1			 7
+#define D6_WT5CCP0			 7
+#define D7_WT5CCP1			 7
 
-E3_AIN0,
+#define D2_USB0EPEN			 8
+#define D3_USB0PFLT			 8
+#define D7_NMI 					 8
 
-E4_AIN9,
-E4_U5Rx,
-E4_I2C2SCL,
-E4_M0PWM4,
-E4_M1PWM2,
-E4_CAN0Rx,
 
-E5_AIN8,
-E5_U5Tx,
-E5_I2C2SDA,
-E5_M0PWM5,
-E5_M1PWM3,
-E5_CAN0Tx,
-/********************************************************	PORTF	********************************************************/	
-F0_U1RTS,
-F0_SSI1Rx,
-F0_CAN0Rx,
-F0_M1PWM4,
-F0_PhA0,
-F0_T0CCP0,
-F0_NMI,
-F0_C0o,
 
-F1_U1CTS,
-F1_SSI1Tx,
-F1_M1PWM5,
-F1_PhB0,
-F1_T0CCP1,
-F1_C1o,
-F1_TRD1,
+#define E0_U7Rx				   1
+#define E1_U7Tx				   1
+#define E4_U5Rx				 	 1
+#define E5_U5Tx				 	 1
 
-F2_SSI1Clk,
-F2_M0FAULT0,
-F2_M1PWM6,
-F2_T1CCP0,
-F2_TRD0,
+#define E4_I2C2SCL			 3
+#define E5_I2C2SDA			 3
 
-F3_SSI1Fss,
-F3_CAN0Tx,
-F3_M1PWM7,
-F3_T1CCP1,
-F3_TRCLK,
+#define E4_M0PWM4			 	 4
+#define E5_M0PWM5			 	 4
 
-F4_M1FAULT0,
-F4_IDX0,
-F4_T2CCP0,
-F4_USB0EPEN,
-}Port_PinModeType;
+#define E4_M1PWM2				 5
+#define E5_M1PWM3				 5
+
+#define E4_CAN0Rx				 8
+#define E5_CAN0Tx				 8
+
+
+#define F0_U1RTS				 1
+#define F1_U1CTS				 1
+
+#define F0_SSI1Rx				 2
+#define F1_SSI1Tx				 2	
+#define F2_SSI1Clk			 2
+#define F3_SSI1Fss		   2
+
+#define F0_CAN0Rx			 	 3
+#define F3_CAN0Tx				 3		 
+
+#define F2_M0FAULT0		 	 4
+
+#define F0_M1PWM4				 5
+#define F1_M1PWM5				 5
+#define F2_M1PWM6				 5
+#define F3_M1PWM7				 5
+#define F4_M1FAULT0			 5
+
+#define F0_PhA0					 6
+#define F1_PhB0					 6
+#define F4_IDX0					 6
+
+#define F0_T0CCP0				 7
+#define F1_T0CCP1				 7
+#define F2_T1CCP0				 7
+#define F3_T1CCP1			 	 7
+#define F4_T2CCP0				 7
+
+#define F0_NMI					 8
+#define F4_USB0EPEN			 8
+
+#define F0_C0o					 9
+#define F1_C1o					 9
+
+#define F1_TRD1					14
+#define F2_TRD0					14
+#define F3_TRCLK				14	
 
 typedef enum
 {
