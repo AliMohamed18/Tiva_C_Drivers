af513526a76bf96090e5ef7289c39fb8fc7cb18f
Ali <alimoh18499@gmail.com>
2020-09-10 21:43:36 +0200

    Update main.c

--- Src/Application/main.c
+++ Src/Application/main.c
@@ -1,10 +1,15 @@
-#include"IntCtrl.h"
+//#include "Mcu.h"
+#include "Port.h"
+#include "Dio.h"
 
 int main(void)
-	{
+ { 	
+
+   Port_Init(Port_Config);
+	 Dio_WriteChannel(Dio_Channel_F4,STD_high);
+
 while(1){
 	
-	
-}
+	}
 return 0;
 }
