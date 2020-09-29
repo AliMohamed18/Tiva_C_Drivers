3a64f220a2b3a0fe3d9e107103a061fb5fd089b7
Ali <alimoh18499@gmail.com>
2020-09-29 18:27:45 +0200

    Update IntCtrl_Lcfg.c

--- Src/Configuration/IntCtrl_Lcfg.c
+++ Src/Configuration/IntCtrl_Lcfg.c
@@ -27,15 +27,14 @@
  *  GLOBAL DATA
  *********************************************************************************************************************/
 
-const Interrupt_CfgType Interrupt_Groups[NUM_OF_ACTIVATED_GROUPS]={
-	 
+const Interrupt_CfgType Interrupt_Groups[NUM_OF_ACTIVATED_GROUPS]={	
 { Interrupt_GPIO_Port_A , 1 , 0 },
 { Interrupt_GPIO_Port_B , 1 , 3 },
 { Interrupt_GPIO_Port_C , 0 , 2 },
 { Interrupt_GPIO_Port_D , 1 , 2 },
 { Interrupt_GPIO_Port_E , 0 , 1 },
 { Interrupt_GPIO_Port_F , 0	, 0 }
-	};
+};
 	
 
 /**********************************************************************************************************************
