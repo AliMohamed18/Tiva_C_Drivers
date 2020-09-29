47f676f8ffa84d8ad9812f879518a7524032ca0f
Ali <alimoh18499@gmail.com>
2020-09-29 19:48:06 +0200

    Update IntCtrl.c

--- Src/Mcal/IntCtrl.c
+++ Src/Mcal/IntCtrl.c
@@ -54,51 +54,48 @@ extern const Interrupt_CfgType  Interrupt_Groups[];
 *******************************************************************************/
 void IntCrtl_Init(void)
 {
-uint8 Counter , PRIGROUP_CHOICE , Group_Temp , SubGroup_Temp , Interrupt_Number_Temp ,Grouping_Field	;
-uint32 priRegOffset	,priBitOffset;
+uint8 Counter , Group_Temp , SubGroup_Temp , Interrupt_Number_Temp ,Grouping_Field	;
+uint32 priRegOffset	,priBitOffset , enRegOffset ,enBitOffset;
 	
 APINT.B.VECTKEY=APINT_VECTKEY; 					 // Field Guard
 APINT.B.ENDIANESS=STD_low;							 //Little-Endian Mode
+	
+	
+APINT.B.PRIGROUP = GROUPING_SYSTEM;      //The Split Of Group Priority From Subpriority
+	
 for(Counter=0 ;Counter < NUM_OF_ACTIVATED_GROUPS;Counter++){
 	
 Interrupt_Number_Temp = Interrupt_Groups[Counter].Interrupt_Number;	
 Group_Temp = Interrupt_Groups[Counter].Group_Priority;
 SubGroup_Temp =	Interrupt_Groups[Counter].SubGroup_Priority;
 
+enRegOffset = (Interrupt_Number_Temp/32)*4;
+enBitOffset = Interrupt_Number_Temp%32;
 	
-#if   ( GROUPING_SYSTEM == 1)
+(*((volatile uint32*)(NVIC_ENX_BASE_ADDRESS + enRegOffset  )))|= (1 << enBitOffset);
 	
-	PRIGROUP_CHOICE = XXX;
+#if   ( GROUPING_SYSTEM == GROUPING_SYSTEM_XXX)
+	
 	Grouping_Field=Group_Temp ;
 	
-#elif ( GROUPING_SYSTEM == 2)
+#elif ( GROUPING_SYSTEM == GROUPING_SYSTEM_XXY)
 	
-	PRIGROUP_CHOICE = XXY;
 	Grouping_Field=(((Group_Temp<<1)&(0x6))|((SubGroup_Temp)&(0x1)));
 
-#elif ( GROUPING_SYSTEM == 3)
+#elif ( GROUPING_SYSTEM == GROUPING_SYSTEM_XYY)
 	
-	PRIGROUP_CHOICE = XYY;	
 	Grouping_Field=(((Group_Temp<<2)&(0x4))|((SubGroup_Temp)&(0x3)));	
 
-#elif ( GROUPING_SYSTEM == 4)
+#elif ( GROUPING_SYSTEM == GROUPING_SYSTEM_YYY)
 	
-	PRIGROUP_CHOICE = YYY;		
 	Grouping_Field= SubGroup_Temp ;
 
 #endif
 
-APINT.B.PRIGROUP = PRIGROUP_CHOICE;        //The Split Of Group Priority From Subpriority
+priRegOffset = (Interrupt_Number_Temp/4)*4;
+priBitOffset = 5 + (8 * (Interrupt_Number_Temp%4));
+(*((volatile uint32*)(NVIC_PRIX_BASE_ADDRESS + priRegOffset  ))) |= ((uint32)(Grouping_Field << priBitOffset));
 
-if	   (Interrupt_Number_Temp == 0	 || Interrupt_Number_Temp < 32  )  SET(EN0,(Interrupt_Number_Temp));
-else if(Interrupt_Number_Temp >= 32  &&	Interrupt_Number_Temp < 64  )  SET(EN1,(Interrupt_Number_Temp-32));
-else if(Interrupt_Number_Temp >= 64  &&	Interrupt_Number_Temp < 96  )  SET(EN2,(Interrupt_Number_Temp-64));
-else if(Interrupt_Number_Temp >= 96  &&	Interrupt_Number_Temp < 128 )  SET(EN3,(Interrupt_Number_Temp-96));
-else if(Interrupt_Number_Temp >= 128 &&	Interrupt_Number_Temp <= 138)  SET(EN4,(Interrupt_Number_Temp-128));
-
-			priRegOffset = (Interrupt_Number_Temp/4)*32;
-			priBitOffset = 5 + (8 * (Interrupt_Number_Temp%4));
-			(*((volatile uint32*)(NVIC_PRIX_BASE_ADDRESS + priRegOffset  ))) |= ((uint32)(Grouping_Field << priBitOffset));
 }
 	
 }
