0b63c522594032a49e91af8be7cf66c989043ce9
Ali <alimoh18499@gmail.com>
2020-09-30 17:01:30 +0200

    Update main.c

--- Src/Application/main.c
+++ Src/Application/main.c
@@ -1,10 +1,20 @@
-#include "IntCtrl.h"
-
+#include "Port.h"
+#include "Dio.h"
 int main(void)
  { 	
-IntCrtl_Init();
 	 
+	*((volatile uint32*)(0x400FE000+0x108))= 0x20; 				// GPIO Unlock 
+ 
+Port_Init(Port_Config);
 while(1){
+	Dio_WriteChannel(Dio_Channel_F2,0);
+	Dio_WriteChannel(Dio_Channel_F1,1);
+	
+for(uint32 i =0 ;i<1000;i++){}
+	Dio_WriteChannel(Dio_Channel_F2,1);
+		Dio_WriteChannel(Dio_Channel_F1,0);
+
+for(uint32 i =0 ;i<1000;i++){}
 
 	}
 return 0;
