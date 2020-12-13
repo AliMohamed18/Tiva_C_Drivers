dd6fa5fef3f877fe4977c62a50a474e27d733be6
Ali <alimoh18499@gmail.com>
2020-12-13 15:37:26 +0200

    Update main.c

--- Src/Application/main.c
+++ Src/Application/main.c
@@ -1,23 +1,29 @@
 #include "Port.h"
 #include "Dio.h"
 #include "Mcu.h"
+#include "Uart.h"
+
 int main(void)
  { 	
-	 
 Mcu_Init(); 
 Port_Init(Port_Config);
+Uart_Init(Uart_Config);
+	 char Button;
+	 char A[]="Ali Is The Here \n";
 while(1){
-	Dio_WriteChannel(Dio_Channel_F2,0);
-	Dio_WriteChannel(Dio_Channel_F1,1);
-	
-for(uint32 i =0 ;i<100000;i++){}
-	Dio_WriteChannel(Dio_Channel_F2,1);
-	Dio_WriteChannel(Dio_Channel_F1,0);
+Button=Dio_ReadChannel(Dio_Channel_F0);
+if(Button==0)	{
+Uart_SendString(Uart0,A);
+//	Uart_SendChar(Uart0,'A');
+//	Uart_SendChar(Uart0,'\n');
+}
+else {
+		Uart_SendChar(Uart0,'C');
+		Uart_SendChar(Uart0,'\n');
 
-for(uint32 i =0 ;i<100000;i++){}
 
-	}
-return 0;
+}
+}
 } 
  
 
