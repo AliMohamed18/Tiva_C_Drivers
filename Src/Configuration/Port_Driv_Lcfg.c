8b8a13249f2592b803afa5a332011886f74e79a5
Ali <alimoh18499@gmail.com>
2021-10-05 22:25:44 +0200

    Update Port_Driv_Lcfg.c

--- Src/Configuration/Port_Driv_Lcfg.c
+++ Src/Configuration/Port_Driv_Lcfg.c
@@ -21,26 +21,24 @@
  *  LOCAL DATA 
  *********************************************************************************************************************/
 
-
 /**********************************************************************************************************************
  *  GLOBAL DATA
  *********************************************************************************************************************/
 
-const Port_ConfigType Port_Config[NUMBER_OF_ACTIVATED_CHANNELS]=
-{
-	/* pin       			  Dir         		Mode     Attach      OutputCurr  */
-	
-	{  Dio_Channel_F2		 ,PORT_PIN_OUT   	,DIO	,DEFAULT   	,CURRENT_NA	},
-	{  Dio_Channel_B0		 ,PORT_PIN_OUT   	,B0_U1Rx	,DEFAULT   	,CURRENT_NA	},
-	{  Dio_Channel_B1		 ,PORT_PIN_OUT   	,B1_U1Tx	,DEFAULT   	,CURRENT_NA	}
-	
- };
+const Port_ConfigType Port_Config[NUMBER_OF_ACTIVATED_CHANNELS] =
+	{
+		/* pin       			  Dir         		Mode     Attach      OutputCurr  */
 
-const Dio_ChannelGroupType RGB={0x0E,1,PORTF};
+		{Dio_Channel_F2, PORT_PIN_OUT, DIO, DEFAULT, CURRENT_NA},
+		{Dio_Channel_B0, PORT_PIN_OUT, B0_U1Rx, DEFAULT, CURRENT_NA},
+		{Dio_Channel_B1, PORT_PIN_OUT, B1_U1Tx, DEFAULT, CURRENT_NA}
+
+};
+
+const Dio_ChannelGroupType RGB = {0x0E, 1, PORTF};
 const Dio_ChannelGroupType KeypadInput;
 const Dio_ChannelGroupType KeypadOutput;
 const Dio_ChannelGroupType Lcd_Data;
- 
 
 /**********************************************************************************************************************
  *  END OF FILE: Port_Lcfg.c
