575973d48b1df527ca5c893fd98aa22837be4ef6
Ali <alimoh18499@gmail.com>
2021-10-05 22:05:41 +0200

    Update Uart.c

--- Src/Mcal/Uart.c
+++ Src/Mcal/Uart.c
@@ -12,23 +12,21 @@
 /**********************************************************************************************************************
  *  INCLUDES
  *********************************************************************************************************************/
-#include "Std_Types.h"
 #include "Uart.h"
+#include "Uart_Cfg.h"
 
 /**********************************************************************************************************************
 *  LOCAL MACROS CONSTANT\FUNCTION
 *********************************************************************************************************************/
 static const uint32 Uart_BaseAddress[8] = {
-UART0_BASE_ADDRESS,
-UART1_BASE_ADDRESS,
-UART2_BASE_ADDRESS,
-UART3_BASE_ADDRESS,
-UART4_BASE_ADDRESS,
-UART5_BASE_ADDRESS,
-UART6_BASE_ADDRESS,
-UART7_BASE_ADDRESS
-};
-extern const Uart_ConfigType Uart_Config[];
+	UART0_BASE_ADDRESS,
+	UART1_BASE_ADDRESS,
+	UART2_BASE_ADDRESS,
+	UART3_BASE_ADDRESS,
+	UART4_BASE_ADDRESS,
+	UART5_BASE_ADDRESS,
+	UART6_BASE_ADDRESS,
+	UART7_BASE_ADDRESS};
 
 /**********************************************************************************************************************
  *  LOCAL DATA 
@@ -53,7 +51,7 @@ extern const Uart_ConfigType Uart_Config[];
 /**********************************************************************************************************************
  *  GLOBAL FUNCTION PROTOTYPES
  *********************************************************************************************************************/
-  /******************************************************************************
+/******************************************************************************
 * \Syntax          : void Uart_Init ( const Uart_ConfigType* ConfigPtr )        
 * \Description     : initializing the Uart diver module                                   
 *                                                                             
@@ -63,28 +61,35 @@ extern const Uart_ConfigType Uart_Config[];
 * \Parameters (out): None                                                      
 * \Return value:   : None                                 
 *******************************************************************************/
-void Uart_Init ( const Uart_ConfigType* ConfigPtr ){
-Uart_ChannelType								Temp_UartPin;
-BaudRateType			            	Temp_BuadRate;
+void Uart_Init(const Uart_ConfigType *ConfigPtr)
+{
+	Uart_ChannelType Temp_UartPin;
+	BaudRateType Temp_BuadRate;
+	uint32 IBRD=0, FBRD=0;
+	float BR=0;
+	uint8 Counter;
 
-uint8 Counter;
+	for (Counter = 0; Counter < NUMBER_OF_ACTIVATED_UARTS; Counter++)
+	{
+		Temp_UartPin = ConfigPtr[Counter].UARTPIN;
+		Temp_BuadRate = ConfigPtr[Counter].BAUDRATE;
 
-for(Counter=0;Counter<NUMBER_OF_ACTIVATED_UARTS;Counter++){
-Temp_UartPin=ConfigPtr[Counter].UARTPIN;
-Temp_BuadRate=ConfigPtr[Counter].BAUDRATE;
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET  )))  = 0; /* disable UART */
-	
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTIBRD_OFFSET   ))) = 104; /* 16MHz/16=1MHz, 1MHz/104=9600 baud rate */
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTFBRD_OFFSET   ))) = 11; /* fraction part, see Example 4-4 */
-	
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTCC_OFFSET   )))  =0 ;	         /* use system clock */
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTLCRH_OFFSET )))  =0x60 ;	      /* 8-bit, no parity, 1-stop bit, no FIFO */	
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET  ))) |= (1<< 8);	 /* enable TXE  */
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET  ))) |= (1<< 9);  /* enable RXE  */
-(*((volatile uint32*)(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET  ))) |= (1<< 0); /* enable UART */	
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET) = 0; /* disable UART */ // HSE IS 0 So CLkDiv = 16
+
+		BR = ((float)(((float)SysClk) / (Temp_BuadRate * 16)));
+		IBRD = ((uint32)BR);
+		FBRD = ((uint32)((((float)(BR - IBRD)) * 64) + 0.5));
+
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTIBRD_OFFSET) = IBRD;	   /* 16MHz/16=1MHz, 1MHz/104=9600 baud rate */
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTFBRD_OFFSET) = FBRD;	   /* fraction part, see Example 4-4 */
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTCC_OFFSET) = 0;		   /* use system clock */
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTLCRH_OFFSET) = 0x60;	   /* 8-bit, no parity, 1-stop bit, no FIFO */
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET) |= (1 << 8); /* enable TXE  */
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET) |= (1 << 9); /* enable RXE  */
+		HwAccess(Uart_BaseAddress[Temp_UartPin] + UARTCTL_OFFSET) |= (1 << 0); /* enable UART */
+	}
 }
-}
- /******************************************************************************
+/******************************************************************************
 * \Syntax          : void Uart_SendChar(Uart_ChannelType Channel , char Char )       
 * \Description     : Send Char                                                                             
 * \Sync\Async      : Synchronous                                               
@@ -94,14 +99,16 @@ Temp_BuadRate=ConfigPtr[Counter].BAUDRATE;
 * \Return value:   : Void                                 
 *******************************************************************************/
 
-void Uart_SendChar(Uart_ChannelType Channel , char Char ){
-	
-while(((*((volatile uint32*)(Uart_BaseAddress[Channel] + UARTFR_OFFSET  ))) & 0x20) != 0){} /* wait until Tx buffer not full */
-(*((volatile uint32*)(Uart_BaseAddress[Channel] + UARTDR_OFFSET  )))=Char; 
+void Uart_SendChar(Uart_ChannelType Channel, char Char)
+{
 
+	while (((HwAccess(Uart_BaseAddress[Channel] + UARTFR_OFFSET)) & 0x20) != 0)
+	{
+	} /* wait until Tx buffer not full */
+	HwAccess(Uart_BaseAddress[Channel] + UARTDR_OFFSET) = Char;
 }
 
- /******************************************************************************
+/******************************************************************************
 * \Syntax          : void Uart_ReceiveChar(Uart_ChannelType Channel , char Char )       
 * \Description     : Send Char                                       
 * \Sync\Async      : Synchronous                                               
@@ -111,15 +118,17 @@ while(((*((volatile uint32*)(Uart_BaseAddress[Channel] + UARTFR_OFFSET  ))) & 0x
 * \Return value:   : char                                 
 *******************************************************************************/
 
-char Uart_ReceiveChar(Uart_ChannelType Channel){
+char Uart_ReceiveChar(Uart_ChannelType Channel)
 {
-char CHAR;
-while(((*((volatile uint32*)(Uart_BaseAddress[Channel] + UARTFR_OFFSET  ))) & 0x10) != 0); /* wait until the buffer is not empty */
-CHAR=((char)(*((volatile uint32*)(Uart_BaseAddress[Channel] + UARTDR_OFFSET  ))));        /* read the received data */
-return CHAR; /* and return it */
+	{
+		char CHAR;
+		while ((HwAccess(Uart_BaseAddress[Channel] + UARTFR_OFFSET) & 0x10) != 0)
+			;																/* wait until the buffer is not empty */
+		CHAR = ((char)HwAccess(Uart_BaseAddress[Channel] + UARTDR_OFFSET)); /* read the received data */
+		return CHAR;														/* and return it */
+	}
 }
-}
- /******************************************************************************
+/******************************************************************************
 * \Syntax          : void Uart_SendString(Uart_ChannelType Channel,const sint8 *Str)       
 * \Description     : Send String                                       
 * \Sync\Async      : Synchronous                                               
@@ -131,13 +140,13 @@ return CHAR; /* and return it */
 void Uart_SendString(Uart_ChannelType Channel, char *Str)
 {
 	uint8 i = 0;
-	while(Str[i] != '\0')
+	while (Str[i] != '\0')
 	{
-		Uart_SendChar(Channel,Str[i]);
+		Uart_SendChar(Channel, Str[i]);
 		i++;
 	}
 }
- /******************************************************************************
+/******************************************************************************
 * \Syntax          : void Uart_SendString(Uart_ChannelType Channel,const sint8 *Str)       
 * \Description     : Send String                                       
 * \Sync\Async      : Synchronous                                               
@@ -147,11 +156,11 @@ void Uart_SendString(Uart_ChannelType Channel, char *Str)
 * \Return value:   : char                               
 *******************************************************************************/
 
-void UART_ReceiveString(Uart_ChannelType Channel, char *Str)
+void Uart_ReceiveString(Uart_ChannelType Channel, char *Str)
 {
 	uint8 i = 0;
 	Str[i] = Uart_ReceiveChar(Channel);
-	while(Str[i] != '#')
+	while (Str[i] != '#')
 	{
 		i++;
 		Str[i] = Uart_ReceiveChar(Channel);
