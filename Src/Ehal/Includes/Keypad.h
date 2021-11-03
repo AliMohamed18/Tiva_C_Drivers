7c79f81b5ef409c615b6cd459cd184059c27bc0e
Ali <alimoh18499@gmail.com>
2021-11-03 22:58:07 +0200

    Create Keypad.h

---
+++ Src/Ehal/Includes/Keypad.h
@@ -0,0 +1,15 @@
+
+#ifndef KEYPAD_H_
+#define KEYPAD_H_
+
+#include "Std_Types.h"
+#include "Dio.h"
+#include "KEYPAD_Cfg.h"
+#include "Port_Driv_Cfg.h"
+
+
+char KeyPad_getPressedKey(void);
+
+
+
+#endif /* KEYPAD_H_ */
